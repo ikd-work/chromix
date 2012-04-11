1b23e83d09548a6614c616e0cf4a143f583fadcf
ikd-work <daisuke.25@hotmail.co.jp>
2012-04-11 19:26:54 +0900

    refactoring

--- zabbix-api.js
+++ zabbix-api.js
@@ -1,136 +1,38 @@
-function getTab(){
-	var tab_str = "<ul>"; 
-	var count = 0;
-	for( var key in localStorage ){
-		if( sessionStorage.getItem("selected") == key ){
-			tab_str += "<li id=tab" + count + " class=selected_tab >" + key + "</li>";
-		}else{
-			tab_str += "<li id=tab" + count + " class=tab >" + key + "</li>";
-		}
-		count++;
-	}
-	tab_str += "</ul>"
-	$("#tab").html(tab_str);
-	$("li.tab").click(function(){
-		sessionStorage.setItem("selected",$(this).text());
-		selectedTabView(sessionStorage.getItem("selected"));
-	});
-}
-
-function updateTime(key){
-	var token = JSON.parse(localStorage.getItem(key)).token;
-	var data = {
-	token:token,
-	checktime:parseInt((new Date)/1000)
-	};
-	var json_data = JSON.stringify(data);
-	localStorage.setItem(key,json_data);
-}
-
-function selectedTabView(selected_tab){
-	for( var key in localStorage ){
-		var token = JSON.parse(localStorage.getItem(key)).token;
-		var checktime = JSON.parse(localStorage.getItem(key)).checktime;
-		if( sessionStorage.getItem("selected") == null ){
-			sessionStorage.setItem("selected",key);
-			getTriggerList(key,token,checktime);
-			getTab();
-		}else if( sessionStorage.getItem("selected") == key ){    
-			getTriggerList(key,token,checktime);
-			getTab();
-		}
-	}
-}
-
-
-function getTriggerList(url,token,checktime){
-	var rpcid = 1;
-	var filter = new Object();
-		filter.status = 0;
-		filter.value = 1;
-	var params = new Object();
-		params.output = "extend";
-		params.expandData = 1;
-		params.limit = 100;
-		params.sortfield = "lastchange";
-		params.sortorder = "DESC";
-		params.filter = filter;
-	getZabbixData(rpcid, url, token, "trigger.get", params);
-}
-
-function unixtimeToDate(ut, TZ) {
-	var tD = new Date( ut * 1000 );
-	tD.setTime( tD.getTime() + (60*60*1000 * TZ) );
-	var yy = tD.getYear();
-	var mm = tD.getMonth() + 1;
-	var dd = tD.getDate();
-	if (yy < 2000) { yy += 1900; }
-	if (mm < 10) { mm = "0" + mm; }
-	if (dd < 10) { dd = "0" + dd; }
-	var time = yy + "/" + mm + "/" + dd + " " + tD.getHours() + ":" + tD.getMinutes() + ":" + tD.getSeconds();
-	return time;
-}
-
-function getAllTrigger(url, token, ckecktime) { // "params"はJSON形式の文字列リテラルかJSONに変換可能なオブジェクト
-	var rpcid = 1;
-	var filter = new Object();
-	    filter.status = 0;
-	    filter.value = 1;
-	var params = new Object();
-	    params.output = "extend";
-	    params.expandData = 1;
-	    params.limit = 100;
-   		params.sortfield = "lastchange";
-		params.sortorder = "DESC";
-	    params.filter = filter;
-	var dataRequest = new Object();
-	dataRequest.params = params;
-	dataRequest.auth = token;
-	dataRequest.jsonrpc = '2.0';
-	dataRequest.id = rpcid;
-	dataRequest.method = "trigger.get";
-	var dataJsonRequest = JSON.stringify(dataRequest);
-	var allTrigger = new Object();
-	var api_url = "http://" + url + "/api_jsonrpc.php";
-	$.ajax({
-		type: 'POST',
-		url: api_url,
-		contentType: 'application/json-rpc',
-		dataType: 'json',
-		processData: false,
-		timeout: 10000,
-		async: false,
-		data: dataJsonRequest,
-		success: function(response){
-			allTrigger = response;
-		},
-		error: function(response){  },
-	});
-	return(allTrigger);
-}
-function refreshTriggerCount(){
-	var counter = 0;
-	for( var key in localStorage ){
-		var token = JSON.parse(localStorage.getItem(key)).token;
-		var checktime = JSON.parse(localStorage.getItem(key)).checktime;
-	//	counter += getTriggerCount(key,token,checktime);
-		var alltrigger = getAllTrigger(key,token,checktime);
-		for(var index in alltrigger.result) {
-			for ( var itemname in alltrigger.result[index]){
-				if( itemname == "lastchange"){
-					if( checktime < alltrigger.result[index][itemname] ){
-						counter++;
-					}
-				}
-			}
-		}
-	}
-	if( counter == 0 ){
-		chrome.browserAction.setBadgeText({text:""});
+function displayLoginBox(){
+	clearMsg();
+	if( $("#login").css('display') == "none" ){
+		$("#login").slideDown("slow");
+		$("#add").text("Close Box");
 	}else{
-		chrome.browserAction.setBadgeText({text:String(counter)});
+		$("#login").slideUp("slow");
+		$("#add").text("Add Zabbix");
 	}
 }
+
+function Login(){
+	var url = $("#url").val();
+	var username = $("#username").val();
+	var password = $("#password").val();
+
+	var auth = getAuth(url,username,password);
+	if( auth.result != null ){
+		if( auth.result == "Connection Error!" ){
+			outputError(auth.result);
+		}else{
+			var data = {
+			token:auth.result,
+			checktime:parseInt((new Date)/1000)
+			};
+			var json_data = JSON.stringify(data);
+			localStorage.setItem(url,json_data);
+			location.reload();
+			outputMsg("Login Success");
+		}
+	}else{
+		outputError("Login Failure!");
+	}
+}
+
 //API Access Authentication
 function getAuth(url, user, password) {
 	var params = {"user":user, "password":password};
@@ -165,67 +67,104 @@ function getAuth(url, user, password) {
     return(authResult); // 認証結果をObjectとして返して"auth.id", "auth.result"で取り出す。
 }
 
-// Access Zabbix API and Get Data
-function getZabbixData(rpcid, url, authid, method, params) { // "params"はJSON形式の文字列リテラルかJSONに変換可能なオブジェクト
-	var dataRequest = new Object();
-	dataRequest.params = params;
-	dataRequest.auth = authid;
-	dataRequest.jsonrpc = '2.0';
-	dataRequest.id = rpcid;
-	dataRequest.method = method;
-	var dataJsonRequest = JSON.stringify(dataRequest);
-	var api_url = "http://" + url + "/api_jsonrpc.php";
-	$.ajax({
-		type: 'POST',
-		url: api_url,
-		contentType: 'application/json-rpc',
-		dataType: 'json',
-		processData: false,
-		data: dataJsonRequest,
-		timeout: 10000,
-		success: function(response){
-			clearMsg();
-			showResult(response,url);
-		},
-		error: function(response){
-			outputError("Connection Error!");
-			$("#datatable").fadeOut("normal",function(){
-				var str = "<table><a href=# onclick=Logout('"+url+"')>Logout</a><br>";
-				str += "<div class=noconnection>Not Connected!</div>";
-				str += "</table><br>";
-				$("#datatable").html(str);
-				$("#datatable").fadeIn();
-			});
-		},
+// Main
+
+function getTab(){
+	var tab_str = "<ul>"; 
+	var count = 0;
+	for( var key in localStorage ){
+		if( sessionStorage.getItem("selected") == key ){
+			tab_str += "<li id=" + convertID(key) + " class=selected_tab >" + "<a href=#>" + key + "</a></li>";
+		}else{
+			tab_str += "<li id=" + convertID(key) + " class=tab >" + "<a href=#>" + key + "</a></li>";
+		}
+		count++;
+	}
+	tab_str += "</ul>"
+	$("#tab").html(tab_str);
+	$("li.tab").click(function(){
+		sessionStorage.setItem("selected",$(this).text());
+		selectedTabView(sessionStorage.getItem("selected"));
 	});
 }
 
-function Logout(key){
-	localStorage.removeItem(key);
-	location.reload();
+function selectedTabView(selected_tab){
+	for( var key in localStorage ){
+		var token = JSON.parse(localStorage.getItem(key)).token;
+		var checktime = JSON.parse(localStorage.getItem(key)).checktime;
+		if( sessionStorage.getItem("selected") == null ){
+			sessionStorage.setItem("selected",key);
+			getTriggerList(key,token,checktime);
+			getTab();
+		}else if( sessionStorage.getItem("selected") == key ){    
+			getTriggerList(key,token,checktime);
+			getTab();
+		}
+	}
 }
 
-function clearMsg(){
-	$("#error").fadeOut("slow");
-	$("#message").fadeOut("slow");
+function getTriggerList(url,token,checktime){
+	var rpcid = 1;
+	var filter = new Object();
+		filter.status = 0;
+		filter.value = 1;
+	var params = new Object();
+		params.output = "extend";
+		params.expandData = 1;
+		params.limit = 100;
+		params.monitored = 1;
+		params.sortfield = "lastchange";
+		params.sortorder = "DESC";
+		params.filter = filter;
+	getZabbixData(rpcid, url, token, "trigger.get", params);
 }
 
-function outputError(msg){
-	$("#error").text(msg);
-	$("#error").fadeIn("slow");
+function updateTime(key){
+	var token = JSON.parse(localStorage.getItem(key)).token;
+	var data = {
+	token:token,
+	checktime:parseInt((new Date)/1000)
+	};
+	var json_data = JSON.stringify(data);
+	localStorage.setItem(key,json_data);
 }
 
-function outputMsg(msg){
-	$("#message").text(msg);
-	$("#message").fadeIn("slow");
+function refreshTriggerCount(){
+	var one_counter = 0;
+	var counter = 0;
+	for( var key in localStorage ){
+		var token = JSON.parse(localStorage.getItem(key)).token;
+		var checktime = JSON.parse(localStorage.getItem(key)).checktime;
+		var alltrigger = getAllTrigger(key,token,checktime);
+		for(var index in alltrigger.result) {
+			for ( var itemname in alltrigger.result[index]){
+				if( itemname == "lastchange"){
+					if( checktime < alltrigger.result[index][itemname] ){
+						one_counter++;
+					}
+				}
+			}
+		}
+		if(one_counter > 0){
+			changeTabDesign(convertID(key));
+		}
+		counter += one_counter;
+		one_counter = 0;
+	}
+	if( counter == 0 ){
+		chrome.browserAction.setBadgeText({text:""});
+	}else{
+		chrome.browserAction.setBadgeText({text:String(counter)});
+	}
 }
 
 
-// 取り出したデータをテーブルとして出力
+// Trigger Table Output
+
 function showResult(response,url){
 	var strTable = "";
 	strTable += "<table>";
-	strTable += "<a href=# onclick=Logout('"+url+"')>Logout</a><br>";
+	strTable += "<div id=logout><a href=# onclick=Logout('"+url+"')>Logout</a></div>";
 	if( response.result == "" ){
 		strTable += "<div class=nodata>No Trouble!</div>";
 	}else{
@@ -261,5 +200,149 @@ function showResult(response,url){
 	});
 	updateTime(url);
 	refreshTriggerCount();
-	
+}
+
+// Access Zabbix API and Get Data
+function getZabbixData(rpcid, url, authid, method, params) { // "params"はJSON形式の文字列リテラルかJSONに変換可能なオブジェクト
+	var dataRequest = new Object();
+		dataRequest.params = params;
+		dataRequest.auth = authid;
+		dataRequest.jsonrpc = '2.0';
+		dataRequest.id = rpcid;
+		dataRequest.method = method;
+	var dataJsonRequest = JSON.stringify(dataRequest);
+	var api_url = "http://" + url + "/api_jsonrpc.php";
+	$.ajax({
+		type: 'POST',
+		url: api_url,
+		contentType: 'application/json-rpc',
+		dataType: 'json',
+		processData: false,
+		data: dataJsonRequest,
+		timeout: 10000,
+		success: function(response){
+			clearMsg();
+			showResult(response,url);
+		},
+		error: function(response){
+			outputError("Connection Error!");
+			$("#datatable").fadeOut("normal",function(){
+				var str = "<table><div id=logout><a id=logout href=# onclick=Logout('"+url+"')>Logout</a></div>";
+				str += "<div class=noconnection>Not Connected!</div>";
+				str += "</table><br>";
+				$("#datatable").html(str);
+				$("#datatable").fadeIn();
+			});
+		},
+	});
+}
+
+// function
+
+function unixtimeToDate(ut, TZ) {
+	var tD = new Date( ut * 1000 );
+	tD.setTime( tD.getTime() + (60*60*1000 * TZ) );
+	var yy = tD.getYear();
+	var mm = tD.getMonth() + 1;
+	var dd = tD.getDate();
+	if (yy < 2000) { yy += 1900; }
+	if (mm < 10) { mm = "0" + mm; }
+	if (dd < 10) { dd = "0" + dd; }
+	var time = yy + "/" + mm + "/" + dd + " " + tD.getHours() + ":" + tD.getMinutes() + ":" + tD.getSeconds();
+	return time;
+}
+
+
+function changeTabDesign(id){
+	$("#"+id).css('border-left','2px solid #ff69b4');
+	$("#"+id).css('border-top','2px solid #ff69b4');
+	$("#"+id).css('border-right','2px solid #ff69b4');
+}
+
+function convertID(key){
+	return key.replace(/\/|\./g,"_");
+}
+
+function Logout(key){
+	localStorage.removeItem(key);
+	location.reload();
+}
+
+function clearMsg(){
+	$("#error").fadeOut("slow");
+	$("#message").fadeOut("slow");
+}
+
+function outputError(msg){
+	$("#error").text(msg);
+	$("#error").fadeIn("slow");
+}
+
+function outputMsg(msg){
+	$("#message").text(msg);
+	$("#message").fadeIn("slow");
+}
+
+/* for Backend.html */
+
+function getAllTrigger(url, token, ckecktime) { // "params"はJSON形式の文字列リテラルかJSONに変換可能なオブジェクト
+	var rpcid = 1;
+	var filter = new Object();
+	    filter.status = 0;
+	    filter.value = 1;
+	var params = new Object();
+	    params.output = "extend";
+	    params.expandData = 1;
+	    params.limit = 100;
+   		params.sortfield = "lastchange";
+		params.sortorder = "DESC";
+	    params.filter = filter;
+	var dataRequest = new Object();
+		dataRequest.params = params;
+		dataRequest.auth = token;
+		dataRequest.jsonrpc = '2.0';
+		dataRequest.id = rpcid;
+		dataRequest.method = "trigger.get";
+	var dataJsonRequest = JSON.stringify(dataRequest);
+	var allTrigger = new Object();
+	var api_url = "http://" + url + "/api_jsonrpc.php";
+	$.ajax({
+		type: 'POST',
+		url: api_url,
+		contentType: 'application/json-rpc',
+		dataType: 'json',
+		processData: false,
+		timeout: 10000,
+		async: false,
+		data: dataJsonRequest,
+		success: function(response){
+			allTrigger = response;
+		},
+		error: function(response){  },
+	});
+	return(allTrigger);
+}
+
+function checkTriggerCount(){
+	var counter = 0;
+	for( var key in localStorage ){
+		var token = JSON.parse(localStorage.getItem(key)).token;
+		var checktime = JSON.parse(localStorage.getItem(key)).checktime;
+		var alltrigger = getAllTrigger(key,token,checktime);
+		for(var index in alltrigger.result) {
+			for ( var itemname in alltrigger.result[index]){
+				if( itemname == "lastchange"){
+					if( checktime < alltrigger.result[index][itemname] ){
+						counter++;
+					}
+				}
+			}
+		}
+	}
+	if( counter == 0 ){
+		chrome.browserAction.setBadgeText({text:""});
+	}else{
+		chrome.browserAction.setBadgeText({text:String(counter)});
+	}
+	setTimeout("checkTriggerCount()",1000*20);
 }
