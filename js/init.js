fe03093f3fed2e14ef7b087de5fa32b5bb3914a1
tis302383 <tis302383@U2009C00233-T1.(none)>
2012-08-21 19:17:48 +0900

    Support manifest ver.2 and fix design to Chrome for Linux

---
+++ js/init.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	setOptions();
+	getTab();
+	$(function() {
+		$("input").css('color', '#999');
+		var input_val = [];
+		$("input").each(function() {
+			input_val.push($(this).val());
+		});
+		$("input").focus(function() {
+			var chk_num = $("input").index(this);
+			var chk_val = $("input").eq(chk_num).val();
+			if(chk_val == input_val[chk_num]) {
+				var def_val = $(this).val();
+				$(this).val('');
+				$(this).css('color', '#333');
+				$(this).blur(function() {
+					if($(this).val() == '') {
+						$(this).val(def_val);
+						$(this).css('color', '#999');
+					}
+				});
+			}
+		});
+	});	
+	$("#add").click(function(){
+		displayLoginBox();
+	});
+	$("#button").click(function(){
+		Login();
+	});
+});
+selectedTabView(sessionStorage.getItem("selected"));
+
+$(document).keydown(function(e){
+
+		var keyCode = e.keyCode;
+		if( (keyCode == 13) && ( $("#login").css('display') != "none" ) ){
+			$("#button").trigger("click");
+		}
+});
+
